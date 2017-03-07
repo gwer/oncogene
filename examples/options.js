5d90e924f59013340b009bce05e5d0be3fbad716
gwer <webholt@gmail.com>
2017-03-07 22:48:11 +0300

    Demo options in separate file.

---
+++ examples/options.js
@@ -0,0 +1,95 @@
+const options = {
+    selector: '.oncogene',
+    steps: [
+        {
+            key: 'idOnly',
+            hint: 'first general hint',
+            variants: [
+                {
+                    hint: 'first-first hint',
+                    code: 'first-first code',
+                    value: 'first-first'
+                },
+                {
+                    hint: 'first-second hint',
+                    code: 'first-second code',
+                    value: 'first-second'
+                }
+            ]
+        },
+        {
+            hint: 'second general hint',
+            variants: [
+                {
+                    hint: 'second-first hint',
+                    code: 'second-first code',
+                    value: true
+                },
+                {
+                    hint: 'second-second hint',
+                    code: 'second-second code',
+                    value: false
+                }
+            ],
+            callback: (config, value) => {
+                config.callbackOnly = value
+                return config
+            }
+        },
+        {
+            key: 'thirdByKey',
+            variants: [
+                {
+                    hint: 'third-first hint',
+                    value: null
+                },
+                {
+                    hint: 'third-second hint',
+                    value: { mayBeObject: true }
+                }
+            ],
+            callback: (config, value) => {
+                config.thirdByCallback = value
+                return config
+            }
+        },
+        {
+            key: 'many.levels.key',
+            variants: [
+                {
+                    hint: 'forth-first hint',
+                    value: 0
+                },
+                {
+                    hint: 'forth-second hint',
+                    value: 1
+                },
+                {
+                    hint: 'forth-third hint',
+                    value: 2
+                }
+            ]
+        },
+        {
+            key: 'wow.many.levels.key',
+            variants: [
+                {
+                    hint: 'fifth-first hint',
+                    value: 0
+                },
+                {
+                    hint: 'fifth-second hint',
+                    value: 1
+                },
+                {
+                    hint: 'fifth-third hint',
+                    value: 2
+                }
+            ]
+        }
+    ],
+    config: {wow: {lol: true}},
+    result: {
+        hint: 'You can use it <b>everywhere</b>!'
+    }
+}
